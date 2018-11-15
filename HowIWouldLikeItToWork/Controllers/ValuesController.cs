d925cb711512ce9d3d432685432b38cf4aa72eff
Dan Ferguson <daniefer@icloud.com>
2018-11-15 09:05:02 -0600

    Add project showing the config not updating as expected

---
+++ HowIWouldLikeItToWork/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace HowIWouldLikeItToWork.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return _configuration.GetValue("urls", "<NULL>").Split(';');
+        }
+    }
+}
---
+++ HowIWouldLikeItToWork/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace HowIWouldLikeItToWork
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((context, builder) => builder.AddJsonFile("hostsettings.json", optional: true));
+    }
+}
